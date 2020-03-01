bc5485d95d57e060416cbc955bd752759acd36cb
krunal <laniyakrunal@gmail.com>
2020-03-01 19:27:44 +0530

    added request logger and db connection improved

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ import { fileLoader, mergeResolvers, mergeTypes } from 'merge-graphql-schemas';
 
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
-import mongoose from 'mongoose';
-import path from 'path';
 import { getUser } from './helper';
 import models from './models';
+import mongoose from 'mongoose';
+import morgan from 'morgan'
+import path from 'path';
 
 const app = express();
 
 const graphqlEndpoint = '/graphql';
+const dbName = 'test';
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 
@@ -17,7 +19,13 @@ const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers'))
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('We are connected to Database'));
+db.once('open', () => {
+  app.listen({ port: 8081 }, () => {
+    // eslint-disable-next-line no-console
+    console.log(`🚀 Server ready at http://localhost:8081${graphqlEndpoint}`);
+  });
+
+});
 
 const server = new ApolloServer({
   typeDefs,
@@ -37,10 +45,7 @@ const server = new ApolloServer({
   },
 });
 
+app.use(morgan('tiny'))
 
 server.applyMiddleware({ app });
-app.listen({ port: 8081 }, () => {
-  // eslint-disable-next-line no-console
-  console.log(`🚀 Server ready at http://localhost:8081${graphqlEndpoint}`);
-  mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
-});
+mongoose.connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true });
